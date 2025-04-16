0720da311f2d822bd0c7d8ff48a8cce0bf6ab395
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2025-04-16 20:59:40 +0530

    update: description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'ngopen',
+  description: 'ngopen is a tool for local development',
 }
 
 export default function RootLayout({
